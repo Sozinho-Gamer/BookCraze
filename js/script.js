61a95cf248ef5e9feb7abfdcb41b704684025ff9
Sozinho-Gamer <86755932+Sozinho-Gamer@users.noreply.github.com>
2025-02-25 20:24:03 -0300

    Delete js directory

--- js/script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('menu-toggle').addEventListener('click', function(event) {
-    event.preventDefault();
-    var menu = document.getElementById('menu');
-    if (menu.classList.contains('hidden')) {
-        menu.classList.remove('hidden');
-    } else {
-        menu.classList.add('hidden');
-    }
-});
